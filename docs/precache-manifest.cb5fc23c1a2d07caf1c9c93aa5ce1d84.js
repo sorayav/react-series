002607f783109ebc95ae5c604b4b18223f7fe48c
Soraya Valle <svalleortega@gmail.com>
2020-04-20 22:08:02 +0200

    Create build/docs folder

---
+++ docs/precache-manifest.cb5fc23c1a2d07caf1c9c93aa5ce1d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521a2e4a6b736247a6010f7f1115ddab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31d81977e3947df9e1f3",
+    "url": "./static/css/main.7038df7b.chunk.css"
+  },
+  {
+    "revision": "80c1916735a9b82fd9b0",
+    "url": "./static/js/2.c5ed2f68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c5ed2f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d81977e3947df9e1f3",
+    "url": "./static/js/main.3d328d55.chunk.js"
+  },
+  {
+    "revision": "d08a6900d835b11399c7",
+    "url": "./static/js/runtime-main.566ebdf7.js"
+  }
+]);
